4c4ccd8f6bc2a9506b301f0f514784ae5daf7c9e
Shaurya Kumar <goldenshaurya@gmail.com>
2023-10-14 21:03:59 -0700

    my first calculator program

---
+++ calculator.py
@@ -0,0 +1,36 @@
+print("please select a operation")
+print("press 1 to add")
+print("press 2 to subtract")
+print("press 3 to divide")
+print("press 4 to multiply")
+print("press 5 to the power of 2")
+operation = input()
+
+    
+if int(operation) == 1:
+    num1 = input("enter first number: ")
+    num2 = input("enter second number: ")
+    print("your answer is " + str(int(num1) +int(num2)) )
+
+elif int(operation) == 2:
+    num1 = input("enter first number: ")
+    num2 = input("enter second number: ")
+    print("your answer is " + str(int(num1) -int(num2)) )
+    
+elif int(operation) == 3:
+    num1 = input("enter first number: ")
+    num2 = input("enter second number: ")
+    print("your answer is " +str(int(num1) /int(num2)) )
+    
+elif int(operation) == 4:
+    num1 = input("enter first number: ")
+    num2 = input("enter second number: ")
+    print("your answer is " +str(int(num1) *int(num2)))
+elif int(operation) == 5:
+     num1 = input("enter number:")
+     print("your answer is " +str(int(num1) * int(num1)))
+else:
+    print("not valid")
+    
+    
+    
